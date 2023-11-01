17157260d87293b8a3aede23123ae8fb7af0b5d4
rita <margosha.mashkova@mail.ru>
2023-11-01 17:39:19 +0400

    generate project

---
+++ src/main/java/com/example/aviacase/AviacaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.aviacase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviacaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviacaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aviacase/AviacaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aviacase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviacaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
